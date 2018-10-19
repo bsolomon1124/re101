ee7d03a1af30dcdcc5b46b6d8a56719e04320968
Brad Solomon <brad.solomon.1124@gmail.com>
2018-10-19 13:05:47 -0700

    PyPI setup: specify markdown content type

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     name='re101',
     description='A back-pocket regex cookbook',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     version='0.3.0',
     author='Brad Solomon',
     author_email='brad.solomon.1124@gmail.com',
